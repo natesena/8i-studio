f4016570c04d5afb63f164bd0ecc320b8693207b
Nathaniel Sena <nate.sena1@gmail.com>
2025-03-09 23:10:34 -0400

    made footer text bigger

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ import { FiMail } from "react-icons/fi";
 const Footer = () => {
   return (
     <div className="fixed bg-black w-[100vw] z-100 text-white bg-neutral- px-2 sm:px-6 py-1 rounded-md bottom-0 left-1/2 transform -translate-x-1/2">
-      <h1 className="text-[10px] sm:text-base md:text-lg text-center">
+      <h1 className="text-[14px] sm:text-base md:text-lg text-center">
         <Link href="/studios" className="relative inline-block no-underline after:absolute after:content-[''] after:bg-[#40FD8C] after:h-[2px] after:w-0 after:left-1/2 after:bottom-0 after:transition-all after:duration-300 hover:after:w-full after:-translate-x-1/2">
           Studios
         </Link>
